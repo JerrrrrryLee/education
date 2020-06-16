0535aff8ab9735d569b7d620d314d88bb8205e5e
JerrrrrryLee <lijianruilbj@163.com>
2020-06-16 14:14:02 +0800

    add cms modeule completing

---
+++ service/service_cms/src/main/java/club/codedrinker/cms/CmsApplication.java
@@ -0,0 +1,16 @@
+package club.codedrinker.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "club.codedrinker")
+
+public class CmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CmsApplication.class,args);
+    }
+}
